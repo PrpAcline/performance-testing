b3ac6004d98343a62ab45794685aaa52987b1a2b
Andrew Cline <acline@precisionplanting.com>
2024-08-20 10:16:04 -0500

    feat(express-postres): initial up route

--- packages/express-postgres/build/main.js
+++ packages/express-postgres/build/main.js
@@ -1,12 +1,10 @@
+// main.ts
 import express from "express";
-
-const app = express();
-const port = process.env.PORT || 3000;
-
+var app = express();
+var port = process.env.PORT || 3e3;
 app.get("/up", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
-
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
